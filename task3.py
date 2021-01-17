415e3cc4594837f5b2c2e97fca2bf0ee8cb5dddd
DimonYarkin <dmitryyarkin@yandex.ru>
2021-01-17 22:04:23 +0300

    код изменен

--- task3.py
+++ task3.py
@@ -13,34 +13,60 @@ deque – это обобщение стеков и очередей.
 from collections import deque
 import timeit
 
-
-def my_list_func():
+def my_list_func_pop():
     my_list = [i for i in range(1000)]
     for i in range(len(my_list)):
         my_list.pop()
+
+def my_list_func_extend():
+    my_list = [i for i in range(1000)]
+    my_list.extend(my_list)
+
+def my_list_func_append():
+    my_list = []
     for i in range(1000):
         my_list.append(i)
-    my_list.clear()
 
+def my_deque_func_extend():
+    my_deque = deque(i for i in range(1000))
+    my_deque.extend(my_deque)
 
-def my_deque_func():
+def my_deque_func_pop():
     my_deque = deque(i for i in range(1000))
     for i in range(len(my_deque)):
         my_deque.popleft()
+
+
+def my_deque_func_append():
+    my_deque = deque()
     for i in range(1000):
         my_deque.appendleft(i)
-    my_deque.clear()
 
 
 print(
-    f'Функция my_list_func() {timeit.timeit("my_list_func()", setup="from __main__ import my_list_func", number=10000)}')
+    f'Функция my_list_func_pop() {timeit.timeit("my_list_func_pop()", setup="from __main__ import my_list_func_pop", number=10000)}')
 print(
-    f'Функция my_deque_func() {timeit.timeit("my_deque_func()", setup="from __main__ import my_deque_func", number=10000)}')
+    f'Функция my_deque_func_pop() {timeit.timeit("my_deque_func_pop()", setup="from __main__ import my_deque_func_pop", number=10000)}')
+
+print(
+    f'Функция my_list_func_append() {timeit.timeit("my_list_func_append()", setup="from __main__ import my_list_func_append", number=10000)}')
+
+print(
+    f'Функция my_deque_func_append() {timeit.timeit("my_deque_func_append()", setup="from __main__ import my_deque_func_append", number=10000)}')
+
+print(
+    f'Функция my_list_func_extend() {timeit.timeit("my_list_func_extend()", setup="from __main__ import my_list_func_extend", number=10000)}')
+print(
+    f'Функция my_deque_func_extend() {timeit.timeit("my_deque_func_extend()", setup="from __main__ import my_deque_func_extend", number=10000)}')
 
 '''
-Замер производительности показал что одни и теже операции в deque
+Замер производительности показал что одни и теже операции в  бощенстве случаев в deque
 происходят медленее
-Функция my_list_func() 1.0296483650017763
-Функция my_deque_func() 1.1512757450109348
+Функция my_list_func_pop() 0.5713647340016905
+Функция my_deque_func_pop() 0.6791876920033246
+Функция my_list_func_append() 0.410452506999718
+Функция my_deque_func_append() 0.38429314400127623
+Функция my_list_func_extend() 0.2568194780033082
+Функция my_deque_func_extend() 0.4088964760012459
 
 '''
